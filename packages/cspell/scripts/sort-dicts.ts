e8aaf2e09be0c1c1d2b33a9530fed4fc572d7bb7
Bruce <recall4056@gmail.com>
2024-04-03 14:59:11 +0800

    fix(cspell): :bug: sort script error

--- packages/cspell/scripts/sort-dicts.ts
+++ packages/cspell/scripts/sort-dicts.ts
@@ -1,19 +1,21 @@
+import Bun from 'bun'
 import { $ } from 'bun'
 import fs from 'node:fs'
 import path from 'node:path'
-import { stdout } from 'node:process'
 
 const DICTS_DIR = 'dicts'
 
 async function sortFile(file: string) {
   const filePath = path.join(DICTS_DIR, file)
-  await $`grep -v '^$' ${filePath} | sort -fu -o ${filePath}`
-  stdout.write(`Dict sorted: ${file}\n`)
+  // Remove empty lines, sort case-insensitive, update file
+  // TODO: It will throw an error when adding `await` to the beginning of the line.
+  $`grep -v '^$' ${filePath} | sort -fu -o ${filePath}`
+  await Bun.write(Bun.stdout, `Dict sorted: ${file}\n`)
 }
 
 await Promise.all(
   fs
     .readdirSync(DICTS_DIR)
-    .filter((file) => file.endsWith('.txt')) // Only .txt files
+    .filter((file) => file.endsWith('.txt')) // Only process .txt files
     .map((file) => sortFile(file))
 )

0a7488be6d4ccba87ff7ee898b8eb437c666337f
Bruce <recall4056@gmail.com>
2024-04-16 21:57:23 +0800

    refactor(cspell): :recycle: sort dict script using typescript instead of bun script

--- packages/cspell/scripts/sort-dicts.ts
+++ packages/cspell/scripts/sort-dicts.ts
@@ -1,17 +1,22 @@
 import fs from 'node:fs'
 import path from 'node:path'
-
-import Bun, { $ } from 'bun'
+import { stdout } from 'node:process'
 
 const DICTS_DIR = 'dicts'
 
 async function sortFile(file: string) {
-  const filePath = path.join(DICTS_DIR, file)
-  // Remove empty lines, sort case-insensitive, update file
-  // TODO: It will throw an error when adding `await` to the beginning of the line.
-  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  $`grep -v '^$' ${filePath} | sort -fu -o ${filePath}`
-  await Bun.write(Bun.stdout, `Dict sorted: ${file}\n`)
+  try {
+    const filePath = path.join(DICTS_DIR, file)
+    const words = fs
+      .readFileSync(filePath, 'utf-8')
+      .split('\n')
+      .filter((value, index, array) => value && array.indexOf(value) === index)
+      .sort()
+    fs.writeFileSync(filePath, words.join('\n'))
+    stdout.write(`Sorted ${file}\n`)
+  } catch (e) {
+    stdout.write(`Error sorting ${file}: ${(e as Error).message}\n`)
+  }
 }
 
 await Promise.all(

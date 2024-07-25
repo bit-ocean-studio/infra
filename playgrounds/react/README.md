0ffe52147db30fb3c91d065acadfca79b96f1e4d
Bruce <recall4056@gmail.com>
2024-07-25 16:56:51 +0800

    chore: :hammer: update

--- playgrounds/react/README.md
+++
@@ -1,7 +0,0 @@
-# React Playground
-
-> React playground for Bit Ocean.
-
-## License
-
-[MIT](/LICENSE) License &copy; 2024 Bit Ocean
--- playgrounds/react/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import ReactSWC from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [ReactSWC()]
-})
